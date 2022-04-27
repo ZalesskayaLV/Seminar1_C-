caf25d26a344078c22bf036cbc2fc51b1389b477
Lyubov Zalesskaya <zalesskayalyubov@gmail.com>
2022-04-28 01:31:27 +0400

    Completed task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number==1)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> понедельник");
+}
+
+if(number==2)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> вторник");
+}
+
+if(number==3)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> среда");
+}
+
+if(number==4)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> четверг");
+}
+
+if(number==5)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> пятница");
+}
+
+if(number==6)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> суббота");
+}
+
+if(number==7)
+{
+    Console.Write(number);
+    Console.WriteLine(" -> воскресенье");
+}
+
+if(number>7)
+{
+    Console.WriteLine("неверное число");
+}
