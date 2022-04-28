4aa7f7312534645761200f5401bf2d25d7f1d336
Lyubov Zalesskaya <zalesskayalyubov@gmail.com>
2022-04-29 00:46:13 +0400

    Completed task 3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу вычисления значения функции возведения числа в квадрат.
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/ 
+
+Console.Write("Введите число X: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = (number*number);
+
+Console.Write("квадрат X: ");
+Console.WriteLine(result);
